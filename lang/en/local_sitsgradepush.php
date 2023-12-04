41b7ac80886d3a6434e999ed822a0ec59452ac1a
Alistair Spark <aspark21@users.noreply.github.com>
2023-12-04 17:31:26 +0000

    Update wording to Marks Transfer
    Marks = SITS terminology for numerical score. 
    Grade = SITS terminology for completion status e.g. Pass/Fail/Deferred/etc
    Aligning with SITS terminology on this.
    
    Push = originally intended as event based push to SITS but now being manual click to have the marks transferred into SITS

--- lang/en/local_sitsgradepush.php
+++ lang/en/local_sitsgradepush.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'SITS Grade Push';
+$string['pluginname'] = 'SITS Marks Transfer';
 $string['privacy:metadata'] = 'This plugin does not store any personal data.';
 $string['settings:apiclientselect'] = 'Select API client';
 $string['settings:apiclient'] = 'API client';
 $string['settings:apiclient:desc'] = 'Choose which API client to use';
-$string['settings:enable'] = 'Enable Grade Push';
-$string['settings:enable:desc'] = 'Enable grade push to SITS';
-$string['settings:enableasync'] = 'Enable Async Grade Push';
-$string['settings:enableasync:desc'] = 'If enabled, grade push will be done by an ad-hoc task';
-$string['settings:enablesublogpush'] = 'Enable Submission Log Push';
-$string['settings:enablesublogpush:desc'] = 'Enable submission log push to SITS';
+$string['settings:enable'] = 'Enable Marks Transfer';
+$string['settings:enable:desc'] = 'Enable Marks Transfer to SITS';
+$string['settings:enableasync'] = 'Enable Async Marks Transfer';
+$string['settings:enableasync:desc'] = 'If enabled, marks transfer will be done by an ad-hoc task';
+$string['settings:enablesublogpush'] = 'Enable Submission Log transfer';
+$string['settings:enablesublogpush:desc'] = 'Enable submission log transfer to SITS';
 $string['settings:generalsettingsheader'] = 'General Settings';
 $string['settings:subpluginheader'] = 'Subplugin Settings';
 $string['settings:moodleastcode'] = 'Moodle AST Codes';
@@ -51,11 +51,11 @@ $string['settings:userprofilefield'] = 'User Profile Field';
 $string['settings:userprofilefield:desc'] = 'User profile field for export staff';
 $string['label:gradepushassessmentselect'] = 'Select SITS assessment to link to';
 $string['label:jumpto'] = 'Jump to: ';
-$string['label:pushall'] = 'Push All';
+$string['label:pushall'] = 'Transfer All';
 $string['label:reassessmentselect'] = 'Re-assessment';
-$string['label:pushgrade'] = 'Push grades';
+$string['label:pushgrade'] = 'Transfer Marks';
 $string['label:ok'] = 'OK';
-$string['label:lastpushtext'] = 'Last scheduled push task {$a->statustext} {$a->date} at {$a->time}';
+$string['label:lastpushtext'] = 'Last scheduled transfer task {$a->statustext} {$a->date} at {$a->time}';
 $string['option:none'] = 'NONE';
 $string['gradepushassessmentselect'] = 'Select SITS assessment';
 $string['gradepushassessmentselect_help'] = 'Select SITS assessment to link to this activity.';
@@ -64,18 +64,18 @@ $string['reassessmentselect_help'] = 'Select YES if it is a re-assessment.';
 $string['subplugintype_sitsapiclient'] = 'API client used for data integration.';
 $string['cachedef_studentspr'] = 'Student\'s SPR code per SITS assessment pattern';
 $string['invalidstudents'] = 'Students not valid for the mapped assessment components';
-$string['pushrecordsexist'] = 'Push records exist';
-$string['pushrecordsnotexist'] = 'No push records';
+$string['pushrecordsexist'] = 'Transfer records exist';
+$string['pushrecordsnotexist'] = 'No transfer records';
 
-// Grade push index page.
-$string['index:header'] = 'Sits Grade Push History';
+// Marks Transfer index page.
+$string['index:header'] = 'SITS Marks Transfer History';
 
-// Grade push dashboard page.
-$string['dashboard:header'] = 'Sits Grade Push Dashboard';
+// Marks Transfer dashboard page.
+$string['dashboard:header'] = 'SITS Marks Transfer Dashboard';
 
 // Select source page.
 $string['selectsource:header'] = 'Select Source';
-$string['selectsource:title'] = 'SITS Grade Push Select Source';
+$string['selectsource:title'] = 'SITS Marks Transfer - Select Source';
 $string['selectsource:existing'] = 'Select an Existing Activity';
 $string['selectsource:new'] = 'Create a New Activity';
 $string['selectsource:gradeitem'] = 'Select a Gradebook Item';
@@ -91,17 +91,17 @@ $string['existingactivity:navbar'] = 'Existing Activity';
 // Error strings.
 $string['error:assessmentmapping'] = 'Assessment mapping is not found. ID: {$a}';
 $string['error:assessmentisnotmapped'] = 'This activity is not mapped to any assessment component.';
-$string['error:componentgradepushed'] = '{$a} cannot be removed because it has grade push records.';
+$string['error:componentgradepushed'] = '{$a} cannot be removed because it has Marks Transfer records.';
 $string['error:componentgrademapped'] = '{$a} had been mapped to another activity.';
 $string['error:pastactivity'] = 'It looks like this course is from a previous academic year, mappings are not allowed.';
 $string['error:mapassessment'] = 'You do not have permission to map assessment.';
-$string['error:pushgradespermission'] = 'You do not have permission to push grades.';
-$string['error:nostudentgrades'] = 'No student grades found.';
+$string['error:pushgradespermission'] = 'You do not have permission to transfer marks.';
+$string['error:nostudentgrades'] = 'No student marks found.';
 $string['error:nostudentfoundformapping'] = 'No student found for this assessment component.';
 $string['error:emptyresponse'] = 'Empty response received when calling {$a}.';
-$string['error:turnitin_numparts'] = 'Turnitin assignment with multiple parts is not supported by Grade Push.';
-$string['error:duplicatedtask'] = 'There is already a push task in queue / processing for this assessment mapping.';
-$string['error:tasknotfound'] = 'Push task not found.';
+$string['error:turnitin_numparts'] = 'Turnitin assignment with multiple parts is not supported by Marks Transfer.';
+$string['error:duplicatedtask'] = 'There is already a transfer task in queue / processing for this assessment mapping.';
+$string['error:tasknotfound'] = 'Transfer task not found.';
 $string['error:multiplemappingsnotsupported'] = 'Multiple assessment component mappings is not supported by {$a}';
 $string['error:studentnotfound'] = 'Student with idnumber {$a->idnumber} not found for component grade {$a->componentgrade}';
 $string['error:coursemodulenotfound'] = 'Course module not found. ID: {$a}';
@@ -110,23 +110,23 @@ $string['error:nomoduledeliveryfound'] = 'No module delivery found.';
 $string['error:no_mab_found'] = 'No assessment component found for this module delivery.';
 $string['error:mab_not_found'] = 'Assessment component not found. ID: {$a}';
 $string['error:assessmentnotfound'] = 'Error getting assessment. ID: {$a}';
-$string['error:mab_has_push_records'] = 'Assessment component mapping cannot be updated as grades have been pushed for {$a}';
+$string['error:mab_has_push_records'] = 'Assessment component mapping cannot be updated as marks have been transfered for {$a}';
 $string['error:no_update_for_same_mapping'] = 'Nothing to update as the assessment component is already mapped to this activity.';
 $string['error:same_map_code_for_same_activity'] = 'An activity cannot be mapped to more than one assessment component with same map code';
 $string['error:missingparams'] = 'Missing parameters.';
 $string['form:alert_no_mab_found'] = 'No assessment components found';
-$string['form:info_turnitin_numparts'] = 'Please note Turnitin assignment with multiple parts is not supported by Grade Push.';
+$string['form:info_turnitin_numparts'] = 'Please note Turnitin assignment with multiple parts is not supported by Marks Transfer.';
 
 // Capability strings.
 $string['sitsgradepush:mapassessment'] = 'Map assessment component to Moodle activity';
-$string['sitsgradepush:pushgrade'] = 'Push grades to SITS';
+$string['sitsgradepush:pushgrade'] = 'Transfer Marks to SITS';
 
 // Task strings.
-$string['task:pushtask:name'] = 'Schedule Push Task';
+$string['task:pushtask:name'] = 'Schedule Transfer Task';
 $string['task:adhoctask'] = 'Adhoc Task';
-$string['task:status:requested'] = 'Push task requested';
-$string['task:requested:success'] = 'Push task requested successfully';
-$string['task:status:queued'] = 'Push task queued';
-$string['task:status:processing'] = 'Push task processing';
+$string['task:status:requested'] = 'Transfer task requested';
+$string['task:requested:success'] = 'Transfer task requested successfully';
+$string['task:status:queued'] = 'Transfer task queued';
+$string['task:status:processing'] = 'Transfer task processing';
 $string['task:status:completed'] = 'completed';
 $string['task:status:failed'] = 'failed';
