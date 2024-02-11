49c4c0efce57c5e38f002838598ea2bcd54cf27a
Alistair Spark <aspark21@users.noreply.github.com>
2024-02-11 22:00:14 +0000

    mark changed after transfer lang update

--- lang/en/local_sitsgradepush.php
+++ lang/en/local_sitsgradepush.php
@@ -81,7 +81,7 @@ $string['index:grade'] = 'Mark';
 $string['index:submissiondate'] = 'Submission date';
 $string['index:lastmarktransfer'] = 'Mark Transferred';
 $string['index:lastsublogtransfer'] = 'Submission Date recorded in SITS';
-$string['index:mark_changed_to'] = 'Mark changed to {$a}';
+$string['index:mark_changed_to'] = 'Mark changed to {$a} after transfer';
 
 // Marks transfer dashboard page.
 $string['dashboard:header'] = 'SITS assessment mapping and mark transfer';
