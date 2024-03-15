d1f6f426f9c6e2a7675a963fb7c7b8b3cc248949
Alistair Spark <aspark21@users.noreply.github.com>
2024-03-15 14:03:56 +0000

    Update support page

--- settings.php
+++ settings.php
@@ -128,7 +128,7 @@ if ($hassiteconfig) {
         $settings->add(new admin_setting_configtext('local_sitsgradepush/support_page_url',
             get_string('settings:support_page_url', 'local_sitsgradepush'),
             get_string('settings:support_page_url:desc', 'local_sitsgradepush'),
-            'https://wiki.ucl.ac.uk/pages/viewpage.action?pageId=306185189',
+            'https://ucldata.atlassian.net/wiki/spaces/MoodleResourceCentre/pages/96567347/FAQs+errors+support#Error-labels-for-Mark-Transfer',
             PARAM_URL,
             50
         ));
