7256613f7a8ad9b6e558a5466d8a1160608f4521
Alex Yeung <aydevworks@gmail.com>
2024-09-30 13:37:27 +0100

    CTP-3412 & CTP-3413 Create overrides for ECs & SORAs

---
+++ classes/extension/sora.php
@@ -0,0 +1,216 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_sitsgradepush\extension;
+
+use local_sitsgradepush\assessment\assessmentfactory;
+
+/**
+ * Class for Summary of Reasonable Adjustments (SORA).
+ *
+ * @package    local_sitsgradepush
+ * @copyright  2024 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Alex Yeung <k.yeung@ucl.ac.uk>
+ */
+class sora extends extension {
+
+    /** @var string Prefix used to create SORA groups */
+    const SORA_GROUP_PREFIX = 'DEFAULT-SORA-';
+
+    /** @var \stdClass Event data in the AWS message */
+    protected \stdClass $eventdata;
+
+    /** @var \stdClass SORA data in the AWS message */
+    protected \stdClass $soradata;
+
+    /** @var int Time extension in seconds, including extra and rest duration */
+    protected int $timeextension;
+
+    /**
+     * Constructor.
+     *
+     * @param string $message
+     */
+    public function __construct(string $message) {
+        parent::__construct($message);
+
+        // Set the SORA properties that we need.
+        $this->set_sora_properties();
+    }
+
+    /**
+     * Return the whole time extension in seconds, including extra and rest duration.
+     *
+     * @return int
+     */
+    public function get_time_extension(): int {
+        return $this->timeextension;
+    }
+
+    /**
+     * Return the extra duration in minutes.
+     *
+     * @return int
+     */
+    public function get_extra_duration(): int {
+        return (int) $this->soradata->extra_duration;
+    }
+
+    /**
+     * Return the rest duration in minutes.
+     *
+     * @return int
+     */
+    public function get_rest_duration(): int {
+        return (int) $this->soradata->rest_duration;
+    }
+
+    /**
+     * Get the SORA group ID. Create the group if it does not exist.
+     * Add the user to the group. Remove the user from other SORA groups.
+     *
+     * @param int $courseid
+     * @param int $userid
+     *
+     * @return int
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
+    public function get_sora_group_id(int $courseid, int $userid): int {
+        global $CFG;
+        require_once($CFG->dirroot . '/group/lib.php');
+
+        // Check group exists.
+        $groupid = groups_get_group_by_name($courseid, $this->get_extension_group_name());
+
+        if (!$groupid) {
+            // Create group.
+            $newgroup = new \stdClass();
+            $newgroup->courseid = $courseid;
+            $newgroup->name = $this->get_extension_group_name();
+            $newgroup->description = '';
+            $newgroup->enrolmentkey = '';
+            $newgroup->picture = 0;
+            $newgroup->visibility = GROUPS_VISIBILITY_MEMBERS;
+            $newgroup->hidepicture = 0;
+            $newgroup->timecreated = time();
+            $newgroup->timemodified = time();
+            $groupid = groups_create_group($newgroup);
+        }
+
+        // Add user to group.
+        if (!groups_add_member($groupid, $userid)) {
+            throw new \moodle_exception('error:cannotaddusertogroup', 'local_sitsgradepush');
+        }
+
+        // Remove user from previous SORA groups.
+        $this->remove_user_from_previous_sora_groups($groupid, $courseid, $userid);
+
+        return $groupid;
+    }
+
+    /**
+     * Get the SORA group name.
+     *
+     * @return string
+     */
+    public function get_extension_group_name(): string {
+        return sprintf(self::SORA_GROUP_PREFIX . '%d', $this->get_extra_duration() + $this->get_rest_duration());
+    }
+
+    /**
+     * Process the extension.
+     *
+     * @return void
+     * @throws \coding_exception
+     * @throws \dml_exception
+     */
+    public function process_extension(): void {
+        // Get all mappings for the student.
+        $mappings = $this->get_mappings_by_userid($this->get_userid());
+
+        // No mappings found.
+        if (empty($mappings)) {
+            return;
+        }
+
+        // Apply the extension to the assessments.
+        foreach ($mappings as $mapping) {
+            try {
+                $assessment = assessmentfactory::get_assessment($mapping->sourcetype, $mapping->sourceid);
+                if ($assessment->is_user_a_participant($this->userid)) {
+                    $assessment->apply_extension($this);
+                }
+            } catch (\Exception $e) {
+                // Consider logging the exception here.
+                continue;
+            }
+        }
+    }
+
+    /**
+     * Remove the user from previous SORA groups.
+     *
+     * @param int $newgroupid The new group ID or the group to keep the user in.
+     * @param int $courseid The course ID.
+     * @param int $userid The user ID.
+     * @return void
+     * @throws \dml_exception
+     */
+    protected function remove_user_from_previous_sora_groups(int $newgroupid, int $courseid, int $userid): void {
+        global $DB;
+
+        // Find all default SORA groups created by marks transfer.
+        $sql = 'SELECT g.id
+                FROM {groups} g
+                WHERE g.courseid = :courseid
+                AND g.name LIKE :name';
+        $params = [
+            'courseid' => $courseid,
+            'name' => self::SORA_GROUP_PREFIX . '%',
+        ];
+        $soragroups = $DB->get_records_sql($sql, $params);
+
+        foreach ($soragroups as $soragroup) {
+            if ($soragroup->id != $newgroupid) {
+                groups_remove_member($soragroup->id, $userid);
+            }
+        }
+    }
+
+    /**
+     * Set the SORA properties.
+     */
+    private function set_sora_properties(): void {
+        global $DB;
+
+        // Set the event data.
+        $this->eventdata = $this->message->event;
+
+        // Set the SORA data.
+        $this->soradata = $this->message->entity->metadata->sora;
+
+        // Find and set the user ID of the student.
+        $idnumber = $this->soradata->person->student_code;
+        $user = $DB->get_record('user', ['idnumber' => $idnumber], 'id', MUST_EXIST);
+        $this->userid = $user->id;
+
+        // Set the time extension in seconds.
+        $this->timeextension = ($this->get_extra_duration() + $this->get_rest_duration()) * MINSECS;
+    }
+}
