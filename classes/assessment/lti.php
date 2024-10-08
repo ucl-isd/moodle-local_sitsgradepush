206853ccf507943fefbd3ef7d2e562a57b85ff8c
David Watson <14983002+watson8@users.noreply.github.com>
2024-10-08 15:06:20 +0100

    CTP-3660 check LTI grade eligibility

--- classes/assessment/lti.php
+++ classes/assessment/lti.php
@@ -32,35 +32,11 @@ class lti extends activity {
      * @return array
      */
     public function get_all_participants(): array {
-        global $DB, $CFG;
-        // For LTI_SETTING_ constants.
-        require_once("$CFG->dirroot/mod/lti/locallib.php");
-
+        if (!self::check_assessment_validity()->valid) {
+            return [];
+        }
         $context = \context_module::instance($this->coursemodule->id);
-        $capability = 'mod/lti:view';
-
-        // Which type of LTI tool is this?
-        $typeid = $this->sourceinstance->typeid;
-        if (!$typeid) {
-            $tool = lti_get_tool_by_url_match($this->sourceinstance->toolurl, $this->sourceinstance->course);
-            if ($tool) {
-                $typeid = $tool->id;
-            }
-        }
-
-        // Has this tool been configured to accept grades globally or not?
-        $acceptgradestool = $DB->get_field(
-            'lti_types_config', 'value', ['typeid' => $typeid, 'name' => 'acceptgrades']
-        );
-        if ($acceptgradestool == LTI_SETTING_ALWAYS) {
-            return get_enrolled_users($context, $capability);
-        } else if ($acceptgradestool == LTI_SETTING_DELEGATE) {
-            // Whether or not grades are accepted is determined at course level.
-            return $this->sourceinstance->instructorchoiceacceptgrades == LTI_SETTING_ALWAYS
-                ? get_enrolled_users($context, $capability)
-                : [];
-        }
-        return [];
+        return get_enrolled_users($context, 'mod/lti:view');
     }
 
     /**
@@ -81,5 +57,40 @@ class lti extends activity {
     public function get_end_date(): ?int {
         // This activity does not have a start date.
         return null;
+    }
+
+    /**
+     * Check assessment is valid for mapping.
+     *
+     * @return \stdClass
+     */
+    public function check_assessment_validity(): \stdClass {
+        global $CFG, $DB;
+
+        // For LTI_SETTING_ constants.
+        require_once("$CFG->dirroot/mod/lti/locallib.php");
+
+        // Which type of LTI tool is this?
+        $typeid = $this->sourceinstance->typeid;
+        if (!$typeid) {
+            $tool = lti_get_tool_by_url_match($this->sourceinstance->toolurl, $this->sourceinstance->course);
+            if ($tool) {
+                $typeid = $tool->id;
+            }
+        }
+
+        // Has this tool been configured to accept grades globally or not?
+        $acceptgradestool = $DB->get_field(
+            'lti_types_config', 'value', ['typeid' => $typeid, 'name' => 'acceptgrades']
+        );
+        if ($acceptgradestool == LTI_SETTING_ALWAYS) {
+            // At system level, LTI grades are set to be sent to gradebook.
+            return parent::check_assessment_validity();
+        } else if ($acceptgradestool == LTI_SETTING_DELEGATE &&
+            $this->sourceinstance->instructorchoiceacceptgrades == LTI_SETTING_ALWAYS) {
+                // Whether or not grades are accepted is delegated to course level, which is set to yes.
+                return parent::check_assessment_validity();
+        }
+        return $this->set_validity_result(false, 'error:lti_no_grades');
     }
 }
